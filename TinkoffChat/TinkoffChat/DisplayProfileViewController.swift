5a34996a438fabec5e5a7f241420c16cc8be5ece
alexchanov <alekseychanov@gmail.com>
2019-03-07 22:41:40 +0300

    Доработка профиля

---
+++ TinkoffChat/TinkoffChat/DisplayProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DisplayProfileViewController.swift
+//  TinkoffChat
+//
+//  Created by Алексей ]Чанов on 07/03/2019.
+//  Copyright © 2019 Алексей Чанов. All rights reserved.
+//
+
+import UIKit
+
+class DisplayProfileViewController: UIViewController {
+
+    @IBOutlet weak var editDescriptionButton: UIButton!
+    @IBOutlet weak var avatartImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
