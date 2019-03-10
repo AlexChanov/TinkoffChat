b98d67e4f8d42036b7867f496b0d4bbf76f8e804
alexchanov <alekseychanov@gmail.com>
2019-03-10 15:06:04 +0300

    Revert "Сoхранение данных версия 1"
    This reverts commit 7f5982daa0345b352003f1a234e4a39456044865.

--- TinkoffChat/TinkoffChat/DisplayProfileViewController.swift
+++ TinkoffChat/TinkoffChat/DisplayProfileViewController.swift
@@ -16,10 +16,7 @@ class DisplayProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-                editDescriptionButton.layer.cornerRadius = editDescriptionButton.frame.size.height*0.2
-                editDescriptionButton.layer.borderWidth = 1.0
-                editDescriptionButton.layer.borderColor = UIColor.black.cgColor
-                editDescriptionButton.backgroundColor = .white
+        // Do any additional setup after loading the view.
     }
     
 
