9c6e02e4cd56b6b7420fa6a0531d31af68f3de3d
alexchanov <alekseychanov@gmail.com>
2019-04-15 23:41:19 +0300

    + Лези варс

--- TinkofSchool/TinkoffChat/Core layer/CoreComponents/CoreDataStack.swift
+++ TinkofSchool/TinkoffChat/Core layer/CoreComponents/CoreDataStack.swift
@@ -15,3 +15,60 @@ protocol CoreDataStack {
     var saveContext: NSManagedObjectContext { get }
 }
 
+class NestedWorkersCoreDataStack: CoreDataStack {
+    static let shared = NestedWorkersCoreDataStack()
+    private init() {}
+
+    private lazy var storeURL: URL = {
+        let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let docUrl = url.appendingPathComponent("DataModel.sqlite")
+        return docUrl
+    }()
+
+    lazy var objectModel: NSManagedObjectModel = {
+        guard let mom = Bundle.main.url(forResource: "DataModel",
+                                        withExtension: "momd") else { fatalError("Can't search the resource") }
+        guard let objectModel = NSManagedObjectModel(contentsOf: mom)
+            else { fatalError("Can't search the object model by this url: \(mom)") }
+        return objectModel
+    }()
+
+    private lazy var persistentCoordinator: NSPersistentStoreCoordinator = {
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: objectModel)
+        do {
+            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType,
+                                               configurationName: nil, at: storeURL, options: nil)
+        } catch let error {
+            assertionFailure("Error due try to add store at \(storeURL) with description \(error.localizedDescription)")
+        }
+        return coordinator
+    }()
+
+    
+    lazy var masterContext: NSManagedObjectContext = {
+        let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        context.persistentStoreCoordinator = persistentCoordinator
+        context.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
+        context.undoManager = nil
+        return context
+    }()
+
+    func performSave(in context: NSManagedObjectContext, completion: CompletionSaveHandler?) {
+        context.perform {
+            if context.hasChanges {
+                do {
+                    try context.save()
+                } catch {
+                    completion?(error)
+                }
+                if let parentContext = context.parent {
+                    self.performSave(in: parentContext, completion: completion)
+                } else {
+                    completion?(nil)
+                }
+            } else {
+                completion?(nil)
+            }
+        }
+    }
+}
